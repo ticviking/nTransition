e32e110c3a3638b1cd1f1fddfeb3a59836b66e40
Tori Hamblin <tori@ticviking.com>
2014-11-03 11:15:10 -0700

    Basic Creation API exists and a Machine can be created

--- eStateMachine/StateMachine.cs
+++ eStateMachine/StateMachine.cs
@@ -7,5 +7,31 @@ namespace eStateMachine
 {
     public class StateMachine
     {
+        public StateMachine(Action<StateMachineConfig> action)
+        {
+            var config = new StateMachineConfig();
+            action(config);
+            Configuration = config;
+        }
+
+        public Boolean Configured { get { return Configuration != null; } }
+        private StateMachineConfig Configuration { get; set; }
+    }
+
+    public class StateMachineConfig
+    {
+        public StateConfig When(int i)
+        {
+            return new StateConfig
+            {
+                When = i
+            };
+        }
+    }
+
+    public struct StateConfig
+    {
+        public int When { get; set; }
+
     }
 }
---
+++ eStateMachineTests/CreateStateMachineTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+using NUnit.Framework;
+using eStateMachine;
+using Shouldly;
+
+namespace eStateMachineTests
+{
+    [TestFixture]
+    public class StateMachineTests
+    {
+        [Test]
+        public void Exist()
+        {
+            var Machine = new StateMachine((c) =>
+            {
+                
+            });
+        }
+
+        [Test]
+        public void AcceptsAConfigAction()
+        {
+            var Machine = new StateMachine((c) =>
+            {
+                c.When(1);
+            });
+
+            Machine.Configured.ShouldBe(true);
+        }
+    }
+}
