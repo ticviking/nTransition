ba6f3e7eff4553c704407138000aa31c69e0d7b1
Tori Hamblin <tori@ticviking.com>
2014-11-03 15:39:48 -0700

    [Red] To be able to use Enums for types we need to use Icomparable not IEqualtable

---
+++ eStateMachineTests/ExampleTurnStileTests.cs
@@ -0,0 +1,24 @@
+﻿using eStateMachine;
+using NUnit.Framework;
+
+namespace eStateMachineTests
+{
+    [TestFixture]
+    class ExampleTurnStileTests
+    {
+        /// <summary>
+        /// This class uses eStateMachine to implement the turnstile example 
+        /// given at http://en.wikipedia.org/wiki/Finite-state_machine
+        /// </summary>
+        class TurnStile
+        {
+            enum TurnstileStates
+            {
+                Locked,
+                Unlocked
+            }
+            private static readonly StateMachine<TurnstileStates> Machine = new StateMachine<TurnstileStates>(c => { });
+        }
+        
+    }
+}
--- eStateMachineTests/UseAStateMachineTests.cs
+++ eStateMachineTests/UseAStateMachineTests.cs
@@ -20,6 +20,12 @@ namespace eStateMachineTests
                 Machine = machine;
             }
 
+            public UsesAStateMachine(StateMachine<T> machine, T initialState)
+            {
+                Machine = machine;
+                _state = initialState;
+            }
+
             public T State
             {
                 get { return _state; }
@@ -35,6 +41,27 @@ namespace eStateMachineTests
             Should.Throw<InvalidTransitionException>(() => t.State = 1);
             // Try the default type. Important since we were adding a Default -> Default transition in the default constructor
             Should.Throw<InvalidTransitionException>(() => t.State = 0);
+        }
+
+        [Test]
+        public void SimpleCircularStateMachineDoesNotThrow()
+        {
+            var TestMachine = new StateMachine<int>(c =>
+            {
+                c.When(1).To(2).Done();
+                c.When(2).To(3).Done();
+                c.When(3).To(1).Done();
+            });
+            var t = new UsesAStateMachine<int>(TestMachine, 1);
+            Should.NotThrow(() =>
+            {
+                t.State = 2;
+                t.State = 3;
+                t.State = 1;
+                t.State = 2;
+                t.State = 3;
+                t.State = 1;
+            });
         }
     }
 }
