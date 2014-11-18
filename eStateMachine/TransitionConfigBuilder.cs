11c2920e8d7aba4d006b68d99a0a7e398b73a6f5
Tori Hamblin <tori@ticviking.com>
2014-11-18 13:40:28 -0700

    re-add derpy merge error.

---
+++ eStateMachine/TransitionConfigBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eStateMachine
+{
+    public class TransitionConfigBuilder<TState> where TState: IComparable 
+    {
+        private IList<StateTransition<TState>> _stateTransitions;
+        private StateTransition<TState> _inProgressTransition;
+
+        public TransitionConfigBuilder()
+        {
+            _stateTransitions = new List<StateTransition<TState>>();
+            _inProgressTransition = new StateTransition<TState>();
+        }
+
+        public IEnumerable<TState> States
+        {
+            get
+            {
+                return _stateTransitions.Select(s => s.WhenState).Union(_stateTransitions.Select(s => s.ToState)).Distinct();
+            }
+        }
+
+        public TransitionConfigBuilder<TState> From(TState whenState)
+        {
+            _inProgressTransition.When(whenState);
+            return this;
+        }
+
+        public TransitionConfigBuilder<TState> To(TState toState)
+        {
+            _inProgressTransition.To(toState);
+            return this;
+        }
+
+        public void Done()
+        {
+            var transition = _inProgressTransition.Done();
+            if(transition != null) _stateTransitions.Add(transition);
+            _inProgressTransition = new StateTransition<TState>();
+        }
+
+        public TState Between(TState current, TState newState)
+        {
+            var stateTransitions = _stateTransitions.Where(s => s.WhenState.CompareTo(current) == 0 && s.ToState.CompareTo( newState) == 0 );
+            if (!stateTransitions.Any() ) throw new InvalidTransitionException("No Such State Transition Exists");
+
+            return newState;
+        }
+    }
+}
