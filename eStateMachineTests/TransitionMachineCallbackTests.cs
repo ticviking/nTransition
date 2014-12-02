f9eb1cc22e1eeba7e086fcdebaac953f805d1123
Tori Hamblin <tori@ticviking.com>
2014-12-01 18:07:07 -0700

    If Clauses

--- eStateMachineTests/TransitionMachineCallbackTests.cs
+++ eStateMachineTests/TransitionMachineCallbackTests.cs
@@ -10,7 +10,7 @@ namespace eStateMachineTests
         [Test]
         public void MachinesUseIfTransitions()
         {
-            var machine = new TransitionMachine<int>((c) =>
+            var machine = new TransitionMachine<int>(c =>
             {
                 c.From(1).To(2).If(() => 1>2).Done();
                 c.From(2).To(1).If(()=> 1<2 ).Done();
@@ -18,6 +18,24 @@ namespace eStateMachineTests
 
             Should.Throw<InvalidTransitionException>(() => machine.Between(1, 2));
             Should.NotThrow(() => machine.Between(2, 1));
+        }
+
+        [Test]
+        public void MultipleIfsAreAllTested()
+        {
+            var machine = new TransitionMachine<int>(c =>
+            {
+                // Only one of these is true
+                c.From(1).To(2).If(() => 1>2).If(() => 1<2).Done();
+                c.From(2).To(3).If(()=> 1<2).If(()=> 2<1).Done();
+                // Both of these are true
+                c.From(2).To(1).If(()=> 1<2 ).If(()=> 2<3).Done();
+            });
+
+            Should.Throw<InvalidTransitionException>(() => machine.Between(1, 2));
+            Should.Throw<InvalidTransitionException>(() => machine.Between(2, 3));
+            Should.NotThrow(() => machine.Between(2, 1));
+            
         }
     }
 }
